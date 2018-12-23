aa6cf093095e101e6c8b2ba54a7609a4f39b062d
Alibekov <43125387+Alibekov@users.noreply.github.com>
2018-12-23 18:44:23 +0600

    Add files via upload

--- php/lx570_options.sql
+++ php/lx570_options.sql
@@ -8,10 +8,15 @@ CREATE TABLE `lx570_options` (
 
 
 INSERT INTO `lx570_options` (`id`, `name`, `descript`, `img`, `price`) VALUES
-(1, 'Colour', 'Choose a colour', '', 1500.00),
-(2, 'Discs', 'Choose a discs', '', 800.00),
-(3, 'Salon', 'Choose a colour of your salon', '', 300.00),
-(4, 'Accessories', 'Choose a accessories', '', 800.00);
+(1, 'Colour', 'Blue', 'blue.jpg', 1500.00),
+(2, 'Colour', 'Black', 'black.jpg', 800.00),
+(3, 'Colour', 'White', 'white.jpg', 300.00),
+(4, 'Colour', 'Grey', 'grey.jpg', 800.00),
+(5, 'Accessories', 'Safety system', 'glonas.jpg', 1500.00),
+(6, 'Accessories', 'Cameras 360', '360.jpg', 800.00),
+(7, 'Accessories', 'Parrotron', 'part.jpg', 300.00),
+(8, 'Discs', 'Radius 20', 'discs1.jpg', 800.00),
+(9, 'Discs', 'Radius 21', 'discs2.jpg', 800.00);
 
 ALTER TABLE `lx570_options`
   ADD PRIMARY KEY (`id`),
